758ae729120c07ca837f8303976171d4abd2da6e
John Balke <john.balke@gmail.com>
2021-05-14 10:41:03 +0100

    White space

--- pages/about/our-story.tsx
+++ pages/about/our-story.tsx
@@ -9,17 +9,19 @@ function OurStory(props: Props) {
   return (
     <>
       <Banner title='Our Story' />
-      <Wrapper maxWidth='70ch'>
-        <p>
-          With our small drilling rig mounted on the back of a very manoeuvrable
-          light truck, we can drill in your back yard (or front yard) to find
-          water for any purpose.
+      <Wrapper maxWidth='70ch' main>
+        <div>
+          <p>
+            With our small drilling rig mounted on the back of a very
+            manoeuvrable light truck, we can drill in your back yard (or front
+            yard) to find water for any purpose.
+          </p>
           <p>
             With our large drilling rig (operated by our sister company) we can
             drill your rock bore for stock, domestic and irrigation use (up to
             300mm diameter)
           </p>
-        </p>
+        </div>
       </Wrapper>
     </>
   );
--- pages/information-request.tsx
+++ pages/information-request.tsx
@@ -2,28 +2,38 @@ import React from 'react';
 import SiteLayout from 'layouts/SiteLayout';
 import styled from 'styled-components';
 import { Wrapper } from 'components/styled';
+import Banner from '../components/Banner';
+import { Theme } from '../styles/theme';
 
-const Welcome = styled.div`
+const Welcome = styled.h2`
   font-size: 5rem;
   font-weight: 700;
+  color: ${Theme.color.primary};
+  margin: 0;
 `;
 
 const SubText = styled.p`
   font-size: 1.5rem;
+  line-height: 1.2em;
 `;
 
 type InformationRequestProps = {};
 
 function InformationRequest(props: InformationRequestProps) {
   return (
-    <Wrapper maxWidth='80ch'>
-      <Welcome>Hello!</Welcome>
-      <SubText>
-        Want more information about bore drilling? One of our drillers will
-        contact you soon to answer any questions you may have and offer an
-        obligation free quote.
-      </SubText>
-    </Wrapper>
+    <>
+      <Banner title='Information Request' />
+      <Wrapper maxWidth='70ch' main>
+        <div>
+          <Welcome>Hello!</Welcome>
+          <SubText>
+            Want more information about bore drilling? We've recieved your
+            request and one of our drillers will contact you soon to answer any
+            questions you may have and offer an obligation free quote!
+          </SubText>
+        </div>
+      </Wrapper>
+    </>
   );
 }
 
