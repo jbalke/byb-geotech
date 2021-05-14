16e757c31cc14293f9e53c4a2178c38509584678
John Balke <john.balke@gmail.com>
2021-05-14 08:55:45 +0100

    Dev mail config

--- emails/mailer.ts
+++ emails/mailer.ts
@@ -1,14 +1,31 @@
 import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST!,
-  port: parseInt(process.env.MAIL_PORT!),
-  auth: {
-    user: process.env.MAIL_USER!,
-    pass: process.env.MAIL_PASSWORD!,
-  },
-});
+let mailConfig;
+if (process.env.NODE_ENV === 'production') {
+  mailConfig = {
+    host: process.env.MAIL_HOST!,
+    port: parseInt(process.env.MAIL_PORT!),
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER!,
+      pass: process.env.MAIL_PASSWORD!,
+    },
+    logger: true,
+  };
+} else {
+  mailConfig = {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '88bf6901e4163c',
+      pass: 'd34f21ffbe398c',
+    },
+    logger: true,
+  };
+}
+
+const transporter = nodemailer.createTransport(mailConfig);
 
 export async function sendEmail(options: Mail.Options) {
   const emailOptions: Mail.Options = {
