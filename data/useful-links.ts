3dbc1489a083fb73ac7a77bf874cccba47be81de
John Balke <john.balke@gmail.com>
2021-05-14 08:55:28 +0100

    Link update

--- data/useful-links.ts
+++ data/useful-links.ts
@@ -2,5 +2,6 @@ export const usefulLinks = [
   { label: 'Home', href: '/' },
   { label: 'FAQ', href: '/faq' },
   { label: 'Bore Search', href: '/bore-search' },
+  { label: 'Request a Quote', href: '/request-a-quote' },
   { label: 'Contact Us', href: '/contact' },
 ];
