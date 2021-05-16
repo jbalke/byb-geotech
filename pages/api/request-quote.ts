acf454a65e00833aae9e2d0d80d7961cbf42845c
John Balke <john.balke@gmail.com>
2021-05-16 09:47:56 +0100

    Quote request form and api

---
+++ pages/api/request-quote.ts
@@ -0,0 +1,70 @@
+import { sendEmail } from 'emails/mailer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import Mail from 'nodemailer/lib/mailer';
+import { EMAIL } from '../../constants';
+import { validateHuman } from 'emails/bores/utils';
+
+interface Data {
+  name: string;
+  email: string;
+  phone: string;
+  street: string;
+  otherSuburb?: string;
+  suburb: string;
+  service: string;
+  comments: string;
+}
+
+async function sendQuoteEmail({
+  name,
+  email,
+  phone,
+  street,
+  suburb,
+  otherSuburb,
+  service,
+  comments,
+}: Data) {
+  console.log({ otherSuburb });
+
+  const emailOptions: Mail.Options = {
+    to: `${EMAIL.MANAGER}`,
+    subject: `Backyard Bores Request for Quote from ${name}`,
+    text: `Service: ${service}
+Suburb: ${suburb} ${otherSuburb ? `(${otherSuburb})` : ''}
+Street: ${street}
+Customer: ${name}
+Email: ${email}
+Phone: ${phone}
+Comments: ${comments}`,
+  };
+
+  return sendEmail(emailOptions);
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const { token } = req.body;
+
+    if (!(await validateHuman(token))) {
+      res.status(400);
+      return res.json({ message: 'recaptcha failed' });
+    }
+
+    try {
+      const emailRes = await sendQuoteEmail(req.body);
+
+      if (emailRes.messageId) {
+        return res.status(200).json({ message: 'message sent' });
+      }
+    } catch (error) {}
+
+    return res.status(500).json({ message: 'message not sent' });
+  }
+  return res
+    .status(400)
+    .json({ message: `Incorrect method: ${req.method}. Did you mean POST?` });
+}
--- pages/request-a-quote.tsx
+++ pages/request-a-quote.tsx
@@ -9,8 +9,11 @@ import {
   SubmitButton,
   Wrapper,
 } from 'components/styled';
+import { useUIState } from 'context/ui-context';
+import { jobTypes, suburbs } from 'data/quote-form';
 import SiteLayout from 'layouts/SiteLayout';
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import {
   Controller,
   NestedValue,
@@ -19,53 +22,15 @@ import {
 } from 'react-hook-form';
 import Select from 'react-select';
 import styled from 'styled-components';
-import isEmail from 'validator/lib/isEmail';
-import isPostalCode from 'validator/lib/isPostalCode';
 import { Theme } from 'styles/theme';
-import { DevTool } from '@hookform/devtools';
+import { client } from 'utils/client';
+import isEmail from 'validator/lib/isEmail';
+import { debug } from '../utils';
 
 type Option = {
   label: string;
   value: string;
 };
-
-const suburbs = [
-  { label: 'Bowen', value: 'bowen' },
-  { label: 'Proserpine', value: 'proserpine' },
-  { label: 'Seaforth', value: 'seaforth' },
-  { label: 'Shoal Point', value: 'shoal point' },
-  { label: 'Bucasia', value: 'bucasia' },
-  { label: 'Blacks Beach', value: 'blacks beach' },
-  { label: 'Slade Point', value: 'Slade Point' },
-  { label: 'Mackay Harbour', value: 'mackay harbour' },
-  { label: 'East Point', value: 'east point' },
-  { label: 'Andergrove', value: 'andergrove' },
-  { label: 'East Mackay', value: 'east mackay' },
-  { label: 'West Mackay', value: 'west mackay' },
-  { label: 'South Mackay', value: 'south mackay' },
-  { label: 'Illawong Beach', value: 'illawong beach' },
-  { label: 'Paget', value: 'paget' },
-  { label: 'Racecourse', value: 'racecourse' },
-  { label: 'Alexandra', value: 'alexandra' },
-  { label: 'Te Kowai', value: 'te kowai' },
-  { label: 'Walkerston', value: 'walkerston' },
-  { label: 'Bakers Creek', value: 'bakers creek' },
-  { label: 'Mcewens Beach', value: 'mcewens beach' },
-  { label: 'Hay Point', value: 'hay point' },
-  { label: 'Half Tide', value: 'half tide' },
-  { label: 'Solonika Beach', value: 'solonika beach' },
-  { label: 'Grasstree Beach', value: 'grasstree beach' },
-  { label: 'Campwin Beach', value: 'campwin beach' },
-  { label: 'Sarina Beach', value: 'sarina beach' },
-  { label: 'Armstrong Beach', value: 'armstrong beach' },
-];
-
-const jobTypes = [
-  { label: 'Drilling a new bore', value: 'newbore' },
-  { label: 'Cleaning an existing bore', value: 'cleanbore' },
-  { label: 'Pump Supply/Repair', value: 'pump' },
-  { label: 'Geotechnical/Environmental Drilling', value: 'geotech' },
-];
 
 const StyledForm = styled.form`
   font-size: 1rem;
@@ -81,6 +46,13 @@ const OtherSuburbInput = styled(StyledInput)`
   margin-top: 5px;
 `;
 
+const RecaptchaContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  align-self: center;
+`;
+
 const StyledSelect = styled(Select)`
   .select__menu {
     color: ${Theme.color.dropDownMenuText};
@@ -92,7 +64,7 @@ interface FormData {
   email: string;
   phone: string;
   street: string;
-  otherSuburb: string;
+  otherSuburb?: string;
   suburb: NestedValue<Option>;
   service: NestedValue<Option>;
   comments: string;
@@ -121,11 +93,43 @@ function RequestQuote(props: Props) {
       otherSuburb: '',
       comments: '',
     },
+    shouldUnregister: true,
   });
   const watchSuburb = watch('suburb');
   const watchService = watch('service');
 
-  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+  const { theme } = useUIState();
+  const recaptchaRef = useRef<ReCAPTCHA>(null!);
+
+  const onSubmit: SubmitHandler<FormData> = async ({
+    suburb,
+    service,
+    ...rest
+  }: FormData) => {
+    console.log(rest);
+
+    setFormStatus('pending');
+    const token = await recaptchaRef.current.executeAsync();
+
+    if (!token) {
+      setFormStatus('fail');
+      return;
+    }
+
+    try {
+      await client('/api/request-quote', {
+        suburb: suburb.label,
+        service: service.label,
+        ...rest,
+        token,
+      });
+      setFormStatus('success');
+    } catch (error) {
+      debug(error.message);
+
+      setFormStatus('fail');
+    }
+  };
 
   const sortedSuburbs = [
     { label: 'Other', value: 'other' },
@@ -133,168 +137,170 @@ function RequestQuote(props: Props) {
       a.label < b.label ? -1 : a.label > b.label ? 1 : 0
     ),
   ];
-  // const otherSuburb = getValues('otherSuburb');
-
-  useEffect(() => {
-    if (watchSuburb?.value !== 'other') {
-      unregister('otherSuburb');
-    }
-  }, [watchSuburb]);
 
   return (
     <>
       <Banner title='Request a Quote' />
       <Wrapper maxWidth='70ch'>
-        <h2>
-          Please complete the form below so we can provide an accurate quote.
-        </h2>
-        <StyledForm onSubmit={handleSubmit(onSubmit)}>
-          <StyledLabel htmlFor='name'>Name</StyledLabel>
-          <StyledInput
-            type='text'
-            id='name'
-            {...register('name', {
-              required: 'Required',
-              minLength: { value: 3, message: 'Too short' },
-              maxLength: { value: 25, message: '25 character limit' },
-            })}
-          />
-          {errors.name && <InputWarning message={errors.name.message!} />}
-
-          <StyledLabel htmlFor='email' aria-describedby='emailDescribe'>
-            Email
-          </StyledLabel>
-          <StyledInput
-            type='email'
-            id='email'
-            {...register('email', {
-              required: 'Required',
-              validate: (value) =>
-                isEmail(value) || 'Not a valid emaill address',
-            })}
-          />
-          <InputSubtext id='emailDescribe'>
-            We'll never spam you or share your email address with anyone else.
-          </InputSubtext>
-          {errors.email && <InputWarning message={errors.email.message!} />}
-
-          <StyledLabel htmlFor='phone'>Phone</StyledLabel>
-          <StyledInput type='text' id='phone' {...register('phone')} />
-          {errors.phone && <InputWarning message={errors.phone.message!} />}
-
-          <StyledLabel htmlFor='street'>Street</StyledLabel>
-          <StyledInput
-            type='text'
-            id='street'
-            placeholder='The address the service is to be supplied'
-            {...register('street', { required: 'Required' })}
-          />
-          {errors.street && <InputWarning message={errors.street.message!} />}
-
-          <StyledLabel htmlFor='suburb'>Suburb</StyledLabel>
-          <Controller
-            name='suburb'
-            control={control}
-            rules={{
-              required: 'please select your suburb',
-            }}
-            render={({ field }) => (
-              <StyledSelect
-                {...field}
-                options={sortedSuburbs}
-                classNamePrefix='select'
-              />
-            )}
-          />
-          {errors.suburb && <InputWarning message={errors.suburb.message!} />}
-
-          {watchSuburb?.value === 'other' && (
-            <>
-              <OtherSuburbInput
+        {formStatus === 'success' ? (
+          <Message type='success'>
+            We've recieved your request and will be in touch soon! Thank you.
+          </Message>
+        ) : (
+          <>
+            <h2>
+              Please complete the form below so we can provide an accurate
+              quote.
+            </h2>
+            <StyledForm onSubmit={handleSubmit(onSubmit)}>
+              <StyledLabel htmlFor='name'>Name</StyledLabel>
+              <StyledInput
                 type='text'
-                id='otherSuburb'
-                placeholder='Please enter your suburb'
-                aria-label='Other Suburb'
-                {...register('otherSuburb', { required: 'Required' })}
+                id='name'
+                {...register('name', {
+                  required: 'Required',
+                  minLength: { value: 3, message: 'Too short' },
+                  maxLength: { value: 25, message: '25 character limit' },
+                })}
               />
-              {errors.otherSuburb && (
-                <InputWarning message={errors.otherSuburb.message!} />
+              {errors.name && <InputWarning message={errors.name.message!} />}
+
+              <StyledLabel htmlFor='email' aria-describedby='emailDescribe'>
+                Email
+              </StyledLabel>
+              <StyledInput
+                type='email'
+                id='email'
+                {...register('email', {
+                  required: 'Required',
+                  validate: (value) =>
+                    isEmail(value) || 'Not a valid emaill address',
+                })}
+              />
+              <InputSubtext id='emailDescribe'>
+                We'll never spam you or share your email address with anyone
+                else.
+              </InputSubtext>
+              {errors.email && <InputWarning message={errors.email.message!} />}
+
+              <StyledLabel htmlFor='phone'>Phone</StyledLabel>
+              <StyledInput type='text' id='phone' {...register('phone')} />
+              {errors.phone && <InputWarning message={errors.phone.message!} />}
+
+              <StyledLabel htmlFor='street'>Street</StyledLabel>
+              <StyledInput
+                type='text'
+                id='street'
+                placeholder='The address the service is to be supplied'
+                {...register('street', { required: 'Required' })}
+              />
+              {errors.street && (
+                <InputWarning message={errors.street.message!} />
               )}
-            </>
-          )}
 
-          {/* <StyledLabel htmlFor='postCode'>Post Code</StyledLabel>
-          <StyledInput
-            type='text'
-            id='postCode'
-            {...register('postCode', {
-              required: 'Required',
-              validate: (value) =>
-                isPostalCode(value, 'AU') || 'Not a valid post code',
-            })}
-          />
-          {errors.postCode && (
-            <InputWarning message={errors.postCode.message!} />
-          )} */}
-
-          {/* <StyledLabel htmlFor='state'>State</StyledLabel>
-          <StyledInput
-            type='text'
-            id='state'
-            disabled
-            {...register('state', { required: 'Required' })}
-          />
-          {errors.state && <InputWarning message={errors.state.message!} />} */}
-
-          <StyledLabel htmlFor='service'>Service</StyledLabel>
-          <Controller
-            name='service'
-            control={control}
-            rules={{ required: 'please select the service' }}
-            render={({ field }) => (
-              <StyledSelect
-                {...field}
-                options={jobTypes}
-                classNamePrefix='select'
+              <StyledLabel htmlFor='suburb'>Suburb</StyledLabel>
+              <Controller
+                name='suburb'
+                control={control}
+                rules={{
+                  required: 'please select your suburb',
+                }}
+                render={({ field }) => (
+                  <StyledSelect
+                    {...field}
+                    options={sortedSuburbs}
+                    classNamePrefix='select'
+                  />
+                )}
               />
-            )}
-          />
-          {errors.service && <InputWarning message={errors.service.message!} />}
-          {watchSuburb?.value === 'other' &&
-            watchService?.value == 'newbore' && (
-              <Message type='warning'>
-                We may not be able to service your area, please submit your
-                request and we will investigate and recommend the most suitable
-                local driller for you.
-              </Message>
-            )}
+              {errors.suburb && (
+                <InputWarning message={errors.suburb.message!} />
+              )}
 
-          <StyledLabel htmlFor='comments' aria-describedby='commentDescribe'>
-            Comments
-          </StyledLabel>
-          <StyledTextarea
-            id='comments'
-            placeholder='Any additional information you want us to know'
-            rows={8}
-            {...register('comments')}
-          />
-          <InputSubtext id='commentDescribe'>
-            Additional information may be specific timeframe requirements or
-            ease of access to the site.
-          </InputSubtext>
+              {watchSuburb?.value === 'other' && (
+                <>
+                  <OtherSuburbInput
+                    type='text'
+                    id='otherSuburb'
+                    placeholder='Please enter your suburb'
+                    aria-label='Other Suburb'
+                    {...register('otherSuburb', { required: 'Required' })}
+                  />
+                  {errors.otherSuburb && (
+                    <InputWarning message={errors.otherSuburb.message!} />
+                  )}
+                </>
+              )}
 
-          <SubmitButton
-            isLoading={formStatus === 'pending'}
-            fullWidth
-            margin='1rem 0 0 0'
-            size='lg'
-            type='submit'
-          >
-            Submit
-          </SubmitButton>
-        </StyledForm>
+              <StyledLabel htmlFor='service'>Service</StyledLabel>
+              <Controller
+                name='service'
+                control={control}
+                rules={{ required: 'please select the service' }}
+                render={({ field }) => (
+                  <StyledSelect
+                    {...field}
+                    options={jobTypes}
+                    classNamePrefix='select'
+                  />
+                )}
+              />
+              {errors.service && (
+                <InputWarning message={errors.service.message!} />
+              )}
+              {watchSuburb?.value === 'other' &&
+                watchService?.value == 'newbore' && (
+                  <Message type='warning'>
+                    We may not be able to service your area, please submit your
+                    request and we will investigate and recommend the most
+                    suitable local driller for you.
+                  </Message>
+                )}
+
+              <StyledLabel
+                htmlFor='comments'
+                aria-describedby='commentDescribe'
+              >
+                Comments
+              </StyledLabel>
+              <StyledTextarea
+                id='comments'
+                placeholder='Any additional information you want us to know'
+                rows={8}
+                {...register('comments')}
+              />
+              <InputSubtext id='commentDescribe'>
+                Additional information may be specific timeframe requirements or
+                ease of access to the site.
+              </InputSubtext>
+
+              <SubmitButton
+                isLoading={formStatus === 'pending'}
+                fullWidth
+                margin='1rem 0 0 0'
+                size='lg'
+                type='submit'
+              >
+                Submit
+              </SubmitButton>
+              {formStatus === 'fail' && (
+                <Message type='danger'>
+                  Request could not be sent, please try again later.
+                </Message>
+              )}
+              <RecaptchaContainer>
+                <ReCAPTCHA
+                  ref={recaptchaRef}
+                  size='invisible'
+                  sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+                  badge={'inline'}
+                  theme={theme}
+                />
+              </RecaptchaContainer>
+            </StyledForm>
+          </>
+        )}
       </Wrapper>
-      <DevTool control={control} />
     </>
   );
 }
