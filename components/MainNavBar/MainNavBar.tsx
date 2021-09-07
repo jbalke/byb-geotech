c98021b1b78e89fafd8aca345a2f63980aafc45a
John Balke <john.balke@gmail.com>
2021-09-07 12:09:04 +0100

    Add Call / Quote to header

--- components/MainNavBar/MainNavBar.tsx
+++ components/MainNavBar/MainNavBar.tsx
@@ -1,4 +1,7 @@
 import { toggleSidebar } from 'actions/ui-actions';
+import LogoImg from 'assets/logo.webp';
+import Button, { StyledLinkButton } from 'components/Button';
+import CallLink from 'components/CallLink';
 import DarkModeToggle from 'components/DarkModeToggle';
 import { StyledLink } from 'components/Link';
 import StyledNextLink from 'components/Link/StyledNextLink';
@@ -6,24 +9,36 @@ import { useUIDispatch } from 'context/ui-context';
 import { navLinks } from 'data/main-navigation';
 import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 import Image from 'next/image';
-import LogoImg from 'assets/logo.webp';
 import React, { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import styled from 'styled-components';
 import { Theme } from 'styles/theme';
-import Button from '../Button';
 import DropdownContainer from './DropdownContainer';
 import NavBar from './NavBar';
 import NavbarItem from './NavBar/NavbarItem';
 
+const MainNav = styled.header`
+  position: relative;
+  font-family: 'Open Sans', sans-serif;
+  background-color: ${Theme.color.textBackground}; //rgba(0, 0, 0, 0.2);
+  color: ${Theme.color.darkGrey};
+  backdrop-filter: blur(5px);
+  transition: ${Theme.color.transition};
+  isolation: isolate;
+  z-index: 99;
+`;
+
 const Container = styled.div`
-  align-items: center;
+  position: relative;
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-between;
+  align-items: center;
+  padding: 5px;
   margin: 0 auto;
-  max-width: ${Theme.width.max};
   width: 100%;
+  max-width: ${Theme.width.max};
+  z-index: 10;
 `;
 
 const DropdownContents = styled(motion.div)<{ itemCount?: number }>`
@@ -59,38 +74,11 @@ const IconLinkWrapper = styled.div`
   }
 `;
 
-const MainNav = styled.header`
-  align-items: center;
-  background-color: ${Theme.color.textBackground}; //rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(5px);
-  color: ${Theme.color.darkGrey};
-  display: flex;
-  font-family: 'Open Sans', sans-serif;
-  height: 60px;
-  padding: 0 10px;
-  transition: ${Theme.color.transition};
-  z-index: 99;
-
-  @media (min-width: ${props => props.theme.bp.desktop}) {
-    height: 75px;
-    padding: 0 30px;
-  }
-
-  ${StyledLink} {
-    box-shadow: none;
-
-    @media (hover: hover) {
-      & {
-        box-shadow: none;
-      }
-    }
-  }
-`;
-
 const NavLogoLink = styled(StyledNextLink)`
-  > img {
-    margin: auto;
-    cursor: pointer;
+  box-shadow: none;
+
+  :hover {
+    box-shadow: none;
   }
 `;
 
@@ -120,6 +108,71 @@ const SectionContainer = styled.div`
 const SectionPageContainer = styled.div`
   display: grid;
   gap: 0.7rem;
+`;
+
+const PhoneQuote = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 0.75em 1em;
+  background-color: ${Theme.color.primaryDark};
+  color: ${Theme.color.white};
+  padding: 0.75em;
+
+  ${StyledLink} {
+    --link-color: ${Theme.color.white};
+    font-weight: 400;
+  }
+
+  ${StyledLinkButton} {
+    text-shadow: none;
+  }
+
+  @media (min-width: 710px) {
+    & {
+      position: absolute;
+      padding: 0;
+      width: max-content;
+      top: 11px;
+      left: 50%;
+      transform: translateX(-50%);
+      background-color: transparent;
+      color: ${Theme.color.primary};
+      z-index: 20;
+    }
+
+    ${StyledLink} {
+      --link-color: ${Theme.color.primary};
+      font-weight: 400;
+    }
+
+    ${StyledLinkButton} {
+      --fg: ${Theme.color.primary};
+      --focus-fg: ${Theme.color.white};
+    }
+  }
+
+  @media (min-width: 1000px) {
+    & {
+      position: static;
+      padding: 0.75em;
+      transform: none;
+      width: auto;
+      color: ${Theme.color.white};
+      background-color: ${Theme.color.primaryDark};
+    }
+
+    ${StyledLink} {
+      --link-color: ${Theme.color.white};
+      font-weight: 400;
+    }
+
+    ${StyledLinkButton} {
+      --fg: ${Theme.color.white};
+      --focus-fg: ${Theme.color.primary};
+    }
+  }
 `;
 
 const MainNavBar = () => {
@@ -197,6 +250,22 @@ const MainNavBar = () => {
           </MobileMenuToggle>
         </MobileControls>
       </Container>
+      <PhoneQuote>
+        <StyledLinkButton
+          href="/request-a-quote"
+          variant="outline-inverse"
+          size="sm"
+        >
+          Request a Free Quote
+        </StyledLinkButton>
+        <div
+          css={`
+            font-size: 1.5rem;
+          `}
+        >
+          <strong>Call</strong> <CallLink />
+        </div>
+      </PhoneQuote>
     </MainNav>
   );
 };
