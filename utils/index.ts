4ad857e4e43304cf5f8bf3538848b2e9ead18420
John Balke <john.balke@gmail.com>
2021-09-08 16:27:03 +0100

    Debug utility takes error object

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export const isServer = () => typeof window === 'undefined';
 
-export function debug(message: any): void {
+export function debug(error: any): void {
   if (process.env.NODE_ENV !== 'production') {
-    console.error(message);
+    console.error(error.message);
   }
 }
