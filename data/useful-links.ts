ccea3b5036ce7fbd62f785c4a8b6a7878364f4b9
John Balke <john.balke@gmail.com>
2021-12-07 18:23:43 +0000

    Fix install tip url

--- data/useful-links.ts
+++ data/useful-links.ts
@@ -2,7 +2,7 @@ export const usefulLinks = [
   { label: 'Home', href: '/' },
   { label: 'FAQ', href: '/faq' },
   { label: 'Water Bore Drilling', href: '/bores' },
-  { label: 'New Bore & Pump Tips', href: '/bores/new-install' },
+  { label: 'New Bore & Pump Tips', href: '/bores/new-install-tips' },
   { label: 'Request a Quote', href: '/request-a-quote' },
   { label: 'Bore Search', href: '/bore-search' },
   { label: 'Contact Us', href: '/contact' },
