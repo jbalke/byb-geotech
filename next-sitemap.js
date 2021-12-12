740cef96513c04b35036cef7ffc72586d95beb34
John Balke <john.balke@gmail.com>
2021-12-12 11:29:28 +0000

    Add exclusions to robots.txt and sitemap.xml

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,12 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', disallow: '/information-request' },
+      { userAgent: '*', allow: '/' },
+    ],
+  },
+  exclude: ['/information-request'],
 };
