6d1b71e06520a43fdb8d3b3e53ced3c498ae5947
John Balke <john.balke@gmail.com>
2024-12-08 12:27:47 +0000

    remove homeowners content on water testing page

--- pages/help/water-testing.tsx
+++ pages/help/water-testing.tsx
@@ -31,69 +31,6 @@ function index(props: Props) {
       description="Flow rate and water quality testing services for your bore water"
       relatedPages={relatedPages}
     >
-      {' '}
-      <PageSection>
-        <h2>Bore Water Quality Testing Services</h2>
-        <p>
-          <b>For Homeowners</b>
-        </p>
-        <p>
-          We can arrange a simple bore water quality test for <b>free</b>*.
-        </p>
-        <p>We can test levels of the following properties:</p>
-        <p>
-          <ul>
-            <li>
-              Salt - Sodium Chloride (NaCl) - in PPM(Parts Per Million).
-              &#8804;150 is great
-            </li>
-            <li>
-              TDS - Total Dissolvable Solids/Salts (sodium, magnesium, calcium,
-              potassium) - in PPM. &#8804;250 is great
-            </li>
-            <li>
-              pH - Power of Hydrogen (acidity or alkalinity). 6.5 to 7.5 is
-              great
-            </li>
-            <li>
-              TDI - Total Dissolvable Iron (Fe) in PPM. &#8804;0.1 is great
-            </li>
-            <li>
-              Conductivity - a measure of the ability of water to pass an
-              electrical current, measured in micro siemens per centimetre.
-              &#8804;400 is great
-            </li>
-            <li>Free Chlorine - FCl in PPM. 0 is great</li>
-            <li>
-              ORP - Oxidation Reduction Potential measures a substance&apos;s
-              ability to oxidise or reduce another substance.
-            </li>
-            <li>
-              Total Hardness - Water hardness is caused by alkaline earths,
-              primarily calcium and magnesium. 0 is great
-            </li>
-            <li>Alkalinity - As a backup to pH. Neutral is great.</li>
-          </ul>
-        </p>
-        <p>
-          *We can conduct these tests for free if we are in your area at the
-          time and don&apos;t need to make a special trip just for your test. If
-          you can wait, we will drop by and do the tests for free the next time
-          we are going past. Otherwise a nominal fee will apply.
-        </p>
-        <p>
-          <b>Comprehensive Drinking Water Tests</b> can be carried out by Mackay
-          Regional Council Laboratory on (07) 4961 9042. Visit the
-          <ExternalLink
-            href="https://www.mackay.qld.gov.au/residents/services/water/mackay_water_and_waste_laboratory_services"
-            target="_blank"
-            rel="noopener noreferrer"
-            label=" MRC Laboratory"
-          />{' '}
-          site for more information.
-        </p>
-      </PageSection>
-      <PageSection></PageSection>
       <PageSection>
         <h2>Bore Water Flow rate / Yield Testing Services</h2>
         <h3>Professional Water Monitoring and Testing Services</h3>
