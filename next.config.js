65e34cc293dc448299803fcc5fe6cb42ee444f71
John Balke <john.balke@gmail.com>
2025-03-11 21:05:04 +0000

    chore: remove babel config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
